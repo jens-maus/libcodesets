65972476ab1f28136a81afe0607c7c21d49ad39e
Jens Maus <mail@jens-maus.de>
2008-12-20 14:32:22 +0000

    - misc: final 6.7 release preparations.

--- src/version.h
+++ src/version.h
@@ -31,8 +31,8 @@
 
 // for setting all necessary version information
 #define LIB_VERSION    6
-#define LIB_REVISION   6
-#define LIB_DATE       "06.07.2008"
+#define LIB_REVISION   7
+#define LIB_DATE       "20.12.2008"
 #define LIB_COPYRIGHT  "Copyright (c) 2005-2008 codesets.library Open Source Team"
 
 // set the LIB_REV_STRING
