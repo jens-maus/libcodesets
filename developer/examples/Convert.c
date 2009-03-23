bd9ed6039b51617d773f2dc7177cd95ce3f1e1b1
Thore Böckelmann <tboeckel@gmx.de>
2009-03-23 11:30:34 +0000

    * developer/examples/Convert.c: made the demo program compilable for OS4.

--- developer/examples/Convert.c
+++ developer/examples/Convert.c
@@ -8,7 +8,19 @@
 /* This is just a very quickly written test, not a full-featured convertor */
 #define BUF_SIZE 102400
 
-struct Library *CodesetsBase;
+struct Library *CodesetsBase = NULL;
+#if defined(__amigaos4__)
+struct CodesetsIFace *ICodesets = NULL;
+#endif
+
+#if defined(__amigaos4__)
+#define GETINTERFACE(iface, base)	(iface = (APTR)GetInterface((struct Library *)(base), "main", 1L, NULL))
+#define DROPINTERFACE(iface)			(DropInterface((struct Interface *)iface), iface = NULL)
+#else
+#define GETINTERFACE(iface, base)	TRUE
+#define DROPINTERFACE(iface)
+#endif
+
 struct codeset *srcCodeset;
 struct codeset *destCodeset;
 
@@ -23,50 +35,56 @@ int main(int argc, char **argv)
     fprintf(stderr, "Usage: %s <source codeset> <destination codeset> <source file>\n", argv[0]);
     return 0;
   }
-  CodesetsBase = OpenLibrary("codesets.library", 0);
-  if (!CodesetsBase)
+  if((CodesetsBase = OpenLibrary(CODESETSNAME,CODESETSVER)) &&
+     GETINTERFACE(ICodesets, CodesetsBase))
   {
-    fprintf(stderr, "Failed to open codesets.library!\n");
-    return 0;
-  }
-  srcCodeset = CodesetsFind(argv[1], CSA_FallbackToDefault, FALSE, TAG_DONE);
-  if (srcCodeset)
-  {
-    destCodeset = CodesetsFind(argv[2], CSA_FallbackToDefault, FALSE, TAG_DONE);
-    if (destCodeset)
+    srcCodeset = CodesetsFind(argv[1], CSA_FallbackToDefault, FALSE, TAG_DONE);
+    if (srcCodeset)
     {
-      buf = AllocMem(BUF_SIZE, MEMF_CLEAR);
-
-      if (buf)
+      destCodeset = CodesetsFind(argv[2], CSA_FallbackToDefault, FALSE, TAG_DONE);
+      if (destCodeset)
       {
-	f = fopen(argv[3], "r");
-	if (f)
-	{
-	  fread(buf, BUF_SIZE-1, 1, f);
-	  fclose(f);
-	  destbuf = CodesetsConvertStr(CSA_SourceCodeset, srcCodeset, CSA_DestCodeset, destCodeset,
-				       CSA_Source, buf, CSA_DestLenPtr, &destlen, TAG_DONE);
-	  if (destbuf)
-	  {
-	    fprintf(stderr, "Result length: %lu\n", destlen);
-	    fwrite(destbuf, destlen, 1, stdout);
-	    fputc('\n', stderr);
-	    CodesetsFreeA(destbuf, NULL);
-	  }
-	  else
-	    fprintf(stderr, "Failed to convert text!\n");
-	}
-	FreeMem(buf, BUF_SIZE);
+        buf = AllocMem(BUF_SIZE, MEMF_CLEAR);
+
+        if (buf)
+        {
+          f = fopen(argv[3], "r");
+          if (f)
+          {
+            fread(buf, BUF_SIZE-1, 1, f);
+            fclose(f);
+            destbuf = CodesetsConvertStr(CSA_SourceCodeset, srcCodeset,
+                                         CSA_DestCodeset, destCodeset,
+                                         CSA_Source, buf,
+                                         CSA_DestLenPtr, &destlen,
+                                         TAG_DONE);
+            if (destbuf)
+            {
+              fprintf(stderr, "Result length: %lu\n", destlen);
+              fwrite(destbuf, destlen, 1, stdout);
+              fputc('\n', stderr);
+              CodesetsFreeA(destbuf, NULL);
+            }
+          else
+            fprintf(stderr, "Failed to convert text!\n");
+          }
+          FreeMem(buf, BUF_SIZE);
+        }
+        else
+          fprintf(stderr, "Failed to allocate %d bytes for buffer\n", BUF_SIZE);
       }
       else
-	fprintf(stderr, "Failed to allocate %lu bytes for buffer\n", BUF_SIZE);
+        fprintf(stderr, "Unknown destination codeset %s\n", argv[2]);
     }
     else
-      fprintf(stderr, "Unknown destination codeset %s\n", argv[2]);
+      fprintf(stderr, "Unknown source codeset %s\n", argv[1]);
+
+    DROPINTERFACE(ICodesets);
+    CloseLibrary(CodesetsBase);
   }
   else
-    fprintf(stderr, "Unknown source codeset %s\n", argv[1]);
-  CloseLibrary(CodesetsBase);
+    fprintf(stderr, "Failed to open codesets.library!\n");
+
   return 0;
 }
 
