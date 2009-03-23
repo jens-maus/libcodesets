8536b30d3d2da4c6d7e11513e1ed174c3d5fe074
Thore Böckelmann <tboeckel@gmx.de>
2009-03-23 11:32:54 +0000

    * developer/examples/Convert.c: added the usual information header.

--- developer/examples/Convert.c
+++ developer/examples/Convert.c
@@ -1,3 +1,29 @@
+/***************************************************************************
+
+ codesets.library - Amiga shared library for handling different codesets
+ Copyright (C) 2001-2005 by Alfonso [alfie] Ranieri <alforan@tin.it>.
+ Copyright (C) 2005-2007 by codesets.library Open Source Team
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ codesets.library project: http://sourceforge.net/projects/codesetslib/
+
+ Most of the code included in this file was relicensed from GPL to LGPL
+ from the source code of SimpleMail (http://www.sf.net/projects/simplemail)
+ with full permissions by its authors.
+
+ $Id: b64d.c 126 2008-07-05 16:13:27Z damato $
+
+***************************************************************************/
+
 #include <exec/libraries.h>
 #include <libraries/codesets.h>
 #include <proto/codesets.h>
