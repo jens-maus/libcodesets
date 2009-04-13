da9c3b60d62631baf2e104b05aa6c6863255bd83
Pavel Fedin <sonic.amiga@gmail.com>
2009-04-13 08:34:32 +0000

    Copied the file with correct offsets

--- developer/include/defines/codesets.h
+++ developer/include/defines/codesets.h
@@ -22,7 +22,7 @@
                   AROS_LCA(UTF16 **,(__arg3),A2), \
                   AROS_LCA(UTF16 *,(__arg4),A3), \
                   AROS_LCA(ULONG,(__arg5),D0), \
-        struct Library *, (__CodesetsBase), 5, Codesets)
+        struct Library *, (__CodesetsBase), 6, Codesets)
 
 #define CodesetsConvertUTF32toUTF16(arg1, arg2, arg3, arg4, arg5) \
     __CodesetsConvertUTF32toUTF16_WB(CodesetsBase, (arg1), (arg2), (arg3), (arg4), (arg5))
@@ -34,7 +34,7 @@
                   AROS_LCA(UTF32 **,(__arg3),A2), \
                   AROS_LCA(UTF32 *,(__arg4),A3), \
                   AROS_LCA(ULONG,(__arg5),D0), \
-        struct Library *, (__CodesetsBase), 6, Codesets)
+        struct Library *, (__CodesetsBase), 7, Codesets)
 
 #define CodesetsConvertUTF16toUTF32(arg1, arg2, arg3, arg4, arg5) \
     __CodesetsConvertUTF16toUTF32_WB(CodesetsBase, (arg1), (arg2), (arg3), (arg4), (arg5))
@@ -46,7 +46,7 @@
                   AROS_LCA(UTF8 **,(__arg3),A2), \
                   AROS_LCA(UTF8 *,(__arg4),A3), \
                   AROS_LCA(ULONG,(__arg5),D0), \
-        struct Library *, (__CodesetsBase), 7, Codesets)
+        struct Library *, (__CodesetsBase), 8, Codesets)
 
 #define CodesetsConvertUTF16toUTF8(arg1, arg2, arg3, arg4, arg5) \
     __CodesetsConvertUTF16toUTF8_WB(CodesetsBase, (arg1), (arg2), (arg3), (arg4), (arg5))
@@ -55,7 +55,7 @@
         AROS_LC2(BOOL, CodesetsIsLegalUTF8, \
                   AROS_LCA(const UTF8 *,(__arg1),A0), \
                   AROS_LCA(ULONG,(__arg2),D0), \
-        struct Library *, (__CodesetsBase), 8, Codesets)
+        struct Library *, (__CodesetsBase), 9, Codesets)
 
 #define CodesetsIsLegalUTF8(arg1, arg2) \
     __CodesetsIsLegalUTF8_WB(CodesetsBase, (arg1), (arg2))
@@ -64,7 +64,7 @@
         AROS_LC2(BOOL, CodesetsIsLegalUTF8Sequence, \
                   AROS_LCA(const UTF8 *,(__arg1),A0), \
                   AROS_LCA(const UTF8 *,(__arg2),D1), \
-        struct Library *, (__CodesetsBase), 9, Codesets)
+        struct Library *, (__CodesetsBase), 10, Codesets)
 
 #define CodesetsIsLegalUTF8Sequence(arg1, arg2) \
     __CodesetsIsLegalUTF8Sequence_WB(CodesetsBase, (arg1), (arg2))
@@ -76,7 +76,7 @@
                   AROS_LCA(UTF16 **,(__arg3),A2), \
                   AROS_LCA(UTF16 *,(__arg4),A3), \
                   AROS_LCA(ULONG,(__arg5),D0), \
-        struct Library *, (__CodesetsBase), 10, Codesets)
+        struct Library *, (__CodesetsBase), 11, Codesets)
 
 #define CodesetsConvertUTF8toUTF16(arg1, arg2, arg3, arg4, arg5) \
     __CodesetsConvertUTF8toUTF16_WB(CodesetsBase, (arg1), (arg2), (arg3), (arg4), (arg5))
@@ -88,7 +88,7 @@
                   AROS_LCA(UTF8 **,(__arg3),A2), \
                   AROS_LCA(UTF8 *,(__arg4),A3), \
                   AROS_LCA(ULONG,(__arg5),D0), \
-        struct Library *, (__CodesetsBase), 11, Codesets)
+        struct Library *, (__CodesetsBase), 12, Codesets)
 
 #define CodesetsConvertUTF32toUTF8(arg1, arg2, arg3, arg4, arg5) \
     __CodesetsConvertUTF32toUTF8_WB(CodesetsBase, (arg1), (arg2), (arg3), (arg4), (arg5))
@@ -100,7 +100,7 @@
                   AROS_LCA(UTF32 **,(__arg3),A2), \
                   AROS_LCA(UTF32 *,(__arg4),A3), \
                   AROS_LCA(ULONG,(__arg5),D0), \
-        struct Library *, (__CodesetsBase), 12, Codesets)
+        struct Library *, (__CodesetsBase), 13, Codesets)
 
 #define CodesetsConvertUTF8toUTF32(arg1, arg2, arg3, arg4, arg5) \
     __CodesetsConvertUTF8toUTF32_WB(CodesetsBase, (arg1), (arg2), (arg3), (arg4), (arg5))
@@ -109,7 +109,7 @@
         AROS_LC2(struct codeset *, CodesetsSetDefaultA, \
                   AROS_LCA(STRPTR,(__arg1),A0), \
                   AROS_LCA(struct TagItem *,(__arg2),A1), \
-        struct Library *, (__CodesetsBase), 13, Codesets)
+        struct Library *, (__CodesetsBase), 14, Codesets)
 
 #define CodesetsSetDefaultA(arg1, arg2) \
     __CodesetsSetDefaultA_WB(CodesetsBase, (arg1), (arg2))
@@ -126,7 +126,7 @@
         AROS_LC2NR(void, CodesetsFreeA, \
                   AROS_LCA(APTR,(__arg1),A0), \
                   AROS_LCA(struct TagItem *,(__arg2),A1), \
-        struct Library *, (__CodesetsBase), 14, Codesets)
+        struct Library *, (__CodesetsBase), 15, Codesets)
 
 #define CodesetsFreeA(arg1, arg2) \
     __CodesetsFreeA_WB(CodesetsBase, (arg1), (arg2))
@@ -142,7 +142,7 @@
 #define __CodesetsSupportedA_WB(__CodesetsBase, __arg1) \
         AROS_LC1(STRPTR *, CodesetsSupportedA, \
                   AROS_LCA(struct TagItem *,(__arg1),A0), \
-        struct Library *, (__CodesetsBase), 15, Codesets)
+        struct Library *, (__CodesetsBase), 16, Codesets)
 
 #define CodesetsSupportedA(arg1) \
     __CodesetsSupportedA_WB(CodesetsBase, (arg1))
@@ -159,7 +159,7 @@
         AROS_LC2(struct codeset *, CodesetsFindA, \
                   AROS_LCA(STRPTR,(__arg1),A0), \
                   AROS_LCA(struct TagItem *,(__arg2),A1), \
-        struct Library *, (__CodesetsBase), 16, Codesets)
+        struct Library *, (__CodesetsBase), 17, Codesets)
 
 #define CodesetsFindA(arg1, arg2) \
     __CodesetsFindA_WB(CodesetsBase, (arg1), (arg2))
@@ -175,7 +175,7 @@
 #define __CodesetsFindBestA_WB(__CodesetsBase, __arg1) \
         AROS_LC1(struct codeset *, CodesetsFindBestA, \
                   AROS_LCA(struct TagItem *,(__arg1),A0), \
-        struct Library *, (__CodesetsBase), 17, Codesets)
+        struct Library *, (__CodesetsBase), 18, Codesets)
 
 #define CodesetsFindBestA(arg1) \
     __CodesetsFindBestA_WB(CodesetsBase, (arg1))
@@ -191,7 +191,7 @@
 #define __CodesetsUTF8Len_WB(__CodesetsBase, __arg1) \
         AROS_LC1(ULONG, CodesetsUTF8Len, \
                   AROS_LCA(const UTF8 *,(__arg1),A0), \
-        struct Library *, (__CodesetsBase), 18, Codesets)
+        struct Library *, (__CodesetsBase), 19, Codesets)
 
 #define CodesetsUTF8Len(arg1) \
     __CodesetsUTF8Len_WB(CodesetsBase, (arg1))
@@ -199,7 +199,7 @@
 #define __CodesetsUTF8ToStrA_WB(__CodesetsBase, __arg1) \
         AROS_LC1(STRPTR, CodesetsUTF8ToStrA, \
                   AROS_LCA(struct TagItem *,(__arg1),A0), \
-        struct Library *, (__CodesetsBase), 19, Codesets)
+        struct Library *, (__CodesetsBase), 20, Codesets)
 
 #define CodesetsUTF8ToStrA(arg1) \
     __CodesetsUTF8ToStrA_WB(CodesetsBase, (arg1))
@@ -215,7 +215,7 @@
 #define __CodesetsUTF8CreateA_WB(__CodesetsBase, __arg1) \
         AROS_LC1(UTF8 *, CodesetsUTF8CreateA, \
                   AROS_LCA(struct TagItem *,(__arg1),A0), \
-        struct Library *, (__CodesetsBase), 20, Codesets)
+        struct Library *, (__CodesetsBase), 21, Codesets)
 
 #define CodesetsUTF8CreateA(arg1) \
     __CodesetsUTF8CreateA_WB(CodesetsBase, (arg1))
@@ -231,7 +231,7 @@
 #define __CodesetsEncodeB64A_WB(__CodesetsBase, __arg1) \
         AROS_LC1(ULONG, CodesetsEncodeB64A, \
                   AROS_LCA(struct TagItem *,(__arg1),A0), \
-        struct Library *, (__CodesetsBase), 21, Codesets)
+        struct Library *, (__CodesetsBase), 22, Codesets)
 
 #define CodesetsEncodeB64A(arg1) \
     __CodesetsEncodeB64A_WB(CodesetsBase, (arg1))
@@ -247,7 +247,7 @@
 #define __CodesetsDecodeB64A_WB(__CodesetsBase, __arg1) \
         AROS_LC1(ULONG, CodesetsDecodeB64A, \
                   AROS_LCA(struct TagItem *,(__arg1),A0), \
-        struct Library *, (__CodesetsBase), 22, Codesets)
+        struct Library *, (__CodesetsBase), 23, Codesets)
 
 #define CodesetsDecodeB64A(arg1) \
     __CodesetsDecodeB64A_WB(CodesetsBase, (arg1))
@@ -264,7 +264,7 @@
         AROS_LC2(ULONG, CodesetsStrLenA, \
                   AROS_LCA(STRPTR,(__arg1),A0), \
                   AROS_LCA(struct TagItem *,(__arg2),A1), \
-        struct Library *, (__CodesetsBase), 23, Codesets)
+        struct Library *, (__CodesetsBase), 24, Codesets)
 
 #define CodesetsStrLenA(arg1, arg2) \
     __CodesetsStrLenA_WB(CodesetsBase, (arg1), (arg2))
@@ -280,7 +280,7 @@
 #define __CodesetsIsValidUTF8_WB(__CodesetsBase, __arg1) \
         AROS_LC1(BOOL, CodesetsIsValidUTF8, \
                   AROS_LCA(STRPTR,(__arg1),A0), \
-        struct Library *, (__CodesetsBase), 24, Codesets)
+        struct Library *, (__CodesetsBase), 25, Codesets)
 
 #define CodesetsIsValidUTF8(arg1) \
     __CodesetsIsValidUTF8_WB(CodesetsBase, (arg1))
@@ -290,7 +290,7 @@
                   AROS_LCA(APTR,(__arg1),A0), \
                   AROS_LCA(APTR,(__arg2),A1), \
                   AROS_LCA(struct TagItem *,(__arg3),A2), \
-        struct Library *, (__CodesetsBase), 25, Codesets)
+        struct Library *, (__CodesetsBase), 26, Codesets)
 
 #define CodesetsFreeVecPooledA(arg1, arg2, arg3) \
     __CodesetsFreeVecPooledA_WB(CodesetsBase, (arg1), (arg2), (arg3))
@@ -306,7 +306,7 @@
 #define __CodesetsConvertStrA_WB(__CodesetsBase, __arg1) \
         AROS_LC1(STRPTR, CodesetsConvertStrA, \
                   AROS_LCA(struct TagItem *,(__arg1),A0), \
-        struct Library *, (__CodesetsBase), 26, Codesets)
+        struct Library *, (__CodesetsBase), 27, Codesets)
 
 #define CodesetsConvertStrA(arg1) \
     __CodesetsConvertStrA_WB(CodesetsBase, (arg1))
@@ -322,7 +322,7 @@
 #define __CodesetsListCreateA_WB(__CodesetsBase, __arg1) \
         AROS_LC1(struct codesetList *, CodesetsListCreateA, \
                   AROS_LCA(struct TagItem *,(__arg1),A0), \
-        struct Library *, (__CodesetsBase), 27, Codesets)
+        struct Library *, (__CodesetsBase), 28, Codesets)
 
 #define CodesetsListCreateA(arg1) \
     __CodesetsListCreateA_WB(CodesetsBase, (arg1))
@@ -338,7 +338,7 @@
 #define __CodesetsListDeleteA_WB(__CodesetsBase, __arg1) \
         AROS_LC1(BOOL, CodesetsListDeleteA, \
                   AROS_LCA(struct TagItem *,(__arg1),A0), \
-        struct Library *, (__CodesetsBase), 28, Codesets)
+        struct Library *, (__CodesetsBase), 29, Codesets)
 
 #define CodesetsListDeleteA(arg1) \
     __CodesetsListDeleteA_WB(CodesetsBase, (arg1))
@@ -355,7 +355,7 @@
         AROS_LC2(BOOL, CodesetsListAddA, \
                   AROS_LCA(struct codesetList *,(__arg1),A0), \
                   AROS_LCA(struct TagItem *,(__arg2),A1), \
-        struct Library *, (__CodesetsBase), 29, Codesets)
+        struct Library *, (__CodesetsBase), 30, Codesets)
 
 #define CodesetsListAddA(arg1, arg2) \
     __CodesetsListAddA_WB(CodesetsBase, (arg1), (arg2))
@@ -371,7 +371,7 @@
 #define __CodesetsListRemoveA_WB(__CodesetsBase, __arg1) \
         AROS_LC1(BOOL, CodesetsListRemoveA, \
                   AROS_LCA(struct TagItem *,(__arg1),A0), \
-        struct Library *, (__CodesetsBase), 30, Codesets)
+        struct Library *, (__CodesetsBase), 31, Codesets)
 
 #define CodesetsListRemoveA(arg1) \
     __CodesetsListRemoveA_WB(CodesetsBase, (arg1))
