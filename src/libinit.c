1ea114547101df3462bd644d81ab4796ce73efdf
Thore Böckelmann <tboeckel@gmx.de>
2012-04-03 18:06:48 +0000

    * src/libinit.c: fixed the broken build for all non-AROS platforms.

--- src/libinit.c
+++ src/libinit.c
@@ -307,9 +307,15 @@ STATIC CONST CONST_APTR LibVectors[] =
   #ifdef __MORPHOS__
   (CONST_APTR)FUNCARRAY_32BIT_NATIVE,
   #endif
+  #if defined(__AROS__)
   (CONST_APTR)AROS_SLIB_ENTRY(LibOpen, Codesets),
   (CONST_APTR)AROS_SLIB_ENTRY(LibClose, Codesets),
   (CONST_APTR)AROS_SLIB_ENTRY(LibExpunge, Codesets),
+  #else
+  (CONST_APTR)LibOpen,
+  (CONST_APTR)LibClose,
+  (CONST_APTR)LibExpunge,
+  #endif
   (CONST_APTR)LibNull,
   (CONST_APTR)libvector,
   (CONST_APTR)-1
