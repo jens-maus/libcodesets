9f68a813890bbfab87d45779514c30f9a964fa0e
Jens Maus <mail@jens-maus.de>
2014-04-04 22:15:13 +0000

    * misc: final 6.18 release preparations.

--- src/version.h
+++ src/version.h
@@ -31,8 +31,8 @@
 
 // for setting all necessary version information
 #define LIB_VERSION    6
-#define LIB_REVISION   17
-#define LIB_DATE       "26.01.2014"
+#define LIB_REVISION   18
+#define LIB_DATE       "04.04.2014"
 #define LIB_COPYRIGHT  "Copyright (C) 2005-2014 codesets.library Open Source Team"
 
 // set the LIB_REV_STRING
